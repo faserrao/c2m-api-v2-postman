0566c55f25fb76d429d5f13e1d50568ef905ab60
Frank Serrao <faserrao@gmail.com>
2025-07-10 08:37:28 -0400

    Trigger rebuild

--- scripts/ebnf_to_openapi/ebnf_to_openapi.sh
+++ scripts/ebnf_to_openapi/ebnf_to_openapi.sh
@@ -1,1 +1,2 @@
-python ebnf_to_openapi.py c2m-api-dd_v3_withComments_v2.ebnf demo2.yaml
+# python ebnf_to_openapi_v2.py c2m-api-dd_v3_withComments_v2.ebnf demo2.yaml
+python ebnf_to_openapi_v2.py sample.ebnf sample.yaml
